2de2888ef7e9df95f592087b80001f0923fc88cd
rohan7745 <rohanwaghmare735@gmail.com>
2024-05-11 00:15:24 +0530

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+
+const hotelsData = fs.readFileSync('hotels.json');
+const hotels = JSON.parse(hotelsData);
+
+
+app.get('/hotels', (req, res) => {
+ 
+    const city = req.query.city;
+
+   
+    if (city) {
+        const filteredHotels = hotels.filter(hotel => hotel.city.toLowerCase() === city.toLowerCase());
+        res.json(filteredHotels);
+    } else {
+        
+        res.json(hotels);
+    }
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
